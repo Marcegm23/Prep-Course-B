1f89ef0cb9d64abcf7bb1e3b5ca96b1d018efa28
Marcela Guerrero <marcelaguerrero23@gmail.com>
2022-11-18 11:40:32 -0500

    homewok03

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,38 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
-}
+    var suma = 0 
+    for (var i = 0; i <= 10; i++) {
+    suma= suma + i; }
+    return suma     
+       }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  return array.filter((e) => e%2 === 0)
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  var elevaAlCuadrado=array.map(elemento=>elemento**2)
+  return elevaAlCuadrado
 }
-
+ 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
-}
+  return array.reduce ((prev,next)=>prev+next, array[0]);
+    }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  return num.toString().length
 }
   
   
